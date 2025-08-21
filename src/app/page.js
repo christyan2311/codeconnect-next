012f2fbf63182292d19dd3d3ba0dbf6e387cd31e
Christyan Roberto Silva <christyanroberto31@gmail.com>
2025-08-21 20:04:51 -0300

    adiciona paginação aos posts e melhora o estilo dos links de navegação

--- src/app/page.js
+++ src/app/page.js
@@ -21,6 +21,7 @@ import logger from "@/logger";
 
 import styles from "./page.module.css";
 import Link from "next/link";
+
 async function getAllPost(page) {
   const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`);
   if (!response.ok) {
@@ -31,13 +32,16 @@ async function getAllPost(page) {
   return response.json();
 }
 
-export default async function Home() {
-  const { data: posts, prev, next } = await getAllPost(1);
+export default async function Home({ searchParams }) {
+  const currentPage = searchParams?.page || 1
+  const { data: posts, prev, next } = await getAllPost(currentPage)
   return (
     <main className={styles.grid}>
       {posts.map(post => <CardPost  key={post.id} post={post} /> )}
+      <div className={styles.links}>
         {prev && <Link href={`/?page=${prev}`}> Página Anterior</Link>}
         {next && <Link href={`/?page=${next}`}>Próxima Página</Link>}
+        </div>
     </main>
   );
 }
